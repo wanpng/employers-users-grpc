a4c0141d4bd7f1bcc2cb860f267ca943929752fb
Chelzea Salvador <chelzea.salvador@ubidy.com>
2022-04-28 10:13:59 +0800

    initial setup

---
+++ build/protoc-gen.sh
@@ -0,0 +1,9 @@
+export GOPATH=$(go env GOPATH)
+export PATH=$PATH:$GOPATH/bin
+
+SRC_DIR=$(pwd)
+DST_DIR=$(pwd)
+
+echo "OS:" $OSTYPE "[compiling protocol buffers]"
+
+protoc -I=$SRC_DIR --go_out=$GOPATH/src $SRC_DIR/protos/service/*.proto
