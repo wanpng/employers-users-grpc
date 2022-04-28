7b034807ccadfe5e1cd69111f237836e207a44f2
Chelzea Salvador <chelzea.salvador@ubidy.com>
2022-04-28 10:17:44 +0800

    setup

--- build/protoc-gen.sh
+++ build/protoc-gen.sh
@@ -6,4 +6,12 @@ DST_DIR=$(pwd)
 
 echo "OS:" $OSTYPE "[compiling protocol buffers]"
 
+protoc -I=$SRC_DIR --go_out=$GOPATH/src --java_out=$DST_DIR $SRC_DIR/protos/domain/*.proto
 protoc -I=$SRC_DIR --go_out=$GOPATH/src $SRC_DIR/protos/service/*.proto
+
+if [ $OSTYPE = msys ]
+then
+protoc -I=$SRC_DIR --plugin=protoc-gen-grpc-java=$DST_DIR/build/protoc-gen-grpc-java.exe --java_out=$DST_DIR $SRC_DIR/protos/service/*.proto
+else
+protoc -I=$SRC_DIR --plugin=protoc-gen-grpc-java=$DST_DIR/build/protoc-gen-grpc-java --java_out=$DST_DIR $SRC_DIR/protos/service/*.proto
+fi
